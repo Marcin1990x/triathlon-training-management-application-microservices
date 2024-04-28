a4fd6fac7dc6896acca7f08da94bcf4e30794064
Marcin1990x <marcin.konecki21@gmial.com>
2024-04-28 17:17:11 +0200

    Added simple security with Spring Security.

---
+++ users-service/src/main/java/pl/koneckimarcin/usersservice/security/WebSecurity.java
@@ -0,0 +1,34 @@
+package pl.koneckimarcin.usersservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurity {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+
+        security
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        request -> request
+                                .requestMatchers("/register", "/users/**").permitAll())
+                .httpBasic(Customizer.withDefaults());
+
+        return security.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
