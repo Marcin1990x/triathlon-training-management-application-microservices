7540699f79a382fffdd511b75623d1dce7979bb9
Marcin1990x <marcin.konecki21@gmail.com>
2024-05-22 18:56:13 +0200

    Added cors configuration separately for services - temporary solution. Ultimately cors will be set in api gateway.

--- api-gateway/src/main/java/pl/koneckimarcin/gateway/security/CorsConfig.java
+++ api-gateway/src/main/java/pl/koneckimarcin/gateway/security/CorsConfig.java
@@ -1,32 +1,8 @@
 package pl.koneckimarcin.gateway.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-@Component
+@Configuration
 public class CorsConfig {
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("*"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        return new CorsFilter(corsConfigurationSource());
-    }
+    //to be done
 }
