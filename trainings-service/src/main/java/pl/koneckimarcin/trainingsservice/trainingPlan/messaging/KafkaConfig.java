194c1bf517985353eb6eea6a0546c4d6f5db726f
Marcin1990x <marcin.konecki21@gmail.com>
2024-07-03 18:45:54 +0200

    Configured topic and kafka producer.

---
+++ trainings-service/src/main/java/pl/koneckimarcin/trainingsservice/trainingPlan/messaging/KafkaConfig.java
@@ -0,0 +1,31 @@
+package pl.koneckimarcin.trainingsservice.trainingPlan.messaging;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+    @Value("${topics.training-plan.name}")
+    private String topicName;
+    @Value("${topics.training-plan.partitions}")
+    private int topicPartitions;
+    @Value("${topics.training-plan.replicas}")
+    private int topicReplicas;
+
+    @Bean
+    public NewTopic trainingPlanTopic() {
+        return TopicBuilder
+                .name(topicName)
+                .partitions(topicPartitions)
+                .replicas(topicReplicas)
+                .build();
+    }
+
+
+}
