28d4dd757a7187b3540d9d02456f00ad2943a1b1
Marcin1990x <marcin.konecki21@gmail.com>
2024-07-21 17:54:30 +0200

    Added websocket client.

--- users-service/src/main/resources/data.sql
+++ users-service/src/main/resources/data.sql
@@ -2,3 +2,5 @@ INSERT INTO roles (id, role) VALUES (1, 'ADMIN')
 INSERT INTO roles (id, role) VALUES (2, 'COACH')
 INSERT INTO roles (id, role) VALUES (3, 'ATHLETE')
 INSERT INTO roles (id, role) VALUES (4, 'NEW')
+
+INSERT INTO application_user (username, password, email_address) VALUES ('testtest', 'testtest', 'test@test')
